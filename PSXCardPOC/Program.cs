aa95e850f1bb29fd84cffb40c5ef7b41e1c4021f
Junvic <junvic.valdez@instilledbee.net>
2015-08-02 19:22:22 +0800

    Parsing of header block and directory frames

---
+++ PSXCardPOC/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PSXMMCLibrary;
+
+namespace PSXCardPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MemoryCard mc = new MemoryCard("test.mcr");
+            Encoding shiftJisEncoding = Encoding.GetEncoding(932);
+
+            byte[] mBlock = mc.GetBlock(0);
+
+            Console.WriteLine("Header Block: ");
+            // Values should be "M" "C"
+            Console.WriteLine("Magic values: {0}{1}", (char)(mBlock[0]), (char)(mBlock[1]));
+            // XOR or magic values (should be "OE")
+            Console.WriteLine("Checksum magic values: {0}", mBlock[127].ToString("X2"));
+
+            Console.ReadKey();
+
+            int offset = 128;
+
+            for (int i = 0; i <= 15; ++i)
+            {
+                Console.WriteLine("(#{0}) Directory Frame: ", i);
+
+                Console.WriteLine("Availability: {0}", mBlock[offset++].ToString("X2"));
+
+                Console.Write("Block Reserved Flags:");
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.WriteLine("");
+
+                Console.Write("Used Blocks Byte:");
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.WriteLine("");
+
+                Console.Write("Link order:");
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.Write(" " + mBlock[offset++].ToString("X2"));
+                Console.WriteLine("");
+
+                Console.Write("Country Code:");
+                Console.Write(" " + (char)mBlock[offset++]);
+                Console.Write("" + (char)mBlock[offset++]);
+                Console.WriteLine("");
+
+                Console.WriteLine("Product Code: {0}", shiftJisEncoding.GetString(mBlock, offset, 10));
+                offset += 10;
+                Console.WriteLine("Identifier: {0}", shiftJisEncoding.GetString(mBlock, offset, 8));
+                offset += 8;
+                offset += 97; // unused bytes (length 97/0x61) "00"
+                Console.WriteLine("Checksum: {0}", mBlock[offset++].ToString("X2"));
+
+                Console.ReadKey();
+            }
+
+            for (int i = 1; i <= 15; ++i)
+            {
+                byte[] block = mc.GetBlock(i);
+
+                Console.WriteLine("(#{0}) Title Frame: ", i);
+                Console.WriteLine("Magic values: {0}{1}", (char)(block[0]), (char)(block[1]));
+                Console.WriteLine("Icon display flag: {0}", (int)block[2]);
+                Console.WriteLine("Blocks used: {0}", (int)block[3]);
+                Console.WriteLine("Title: {0}", shiftJisEncoding.GetString(block, 4, 64));
+                Console.WriteLine(String.Empty);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ PSXMMCLibrary/MemoryCard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSXMMCLibrary
+{
+    public class MemoryCard
+    {
+        private static readonly Encoding _shiftJisEncoding = Encoding.GetEncoding(932);
+        private static readonly int _blockSize = 8192;
+
+        private FileStream memCard;
+
+        public MemoryCard(string filepath)
+        {
+            try
+            {
+                memCard = File.Open(filepath, FileMode.Open);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to open file.");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public byte[] GetBlock(int index)
+        {
+            byte[] block = new byte[_blockSize];
+
+            try
+            {
+                memCard.Seek(_blockSize * index, SeekOrigin.Begin);
+                memCard.Read(block, 0, _blockSize);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Unable to read block at index " + index);
+                Console.WriteLine(ex.ToString());
+            }
+
+            return block;
+        }
+
+        public void ShowMagicBlock()
+        {
+            byte[] block = new byte[_blockSize];
+
+            try
+            {
+                block = GetBlock(0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to read header block.");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
