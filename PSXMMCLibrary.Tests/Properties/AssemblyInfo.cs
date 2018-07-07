2735c3a7f5299252361e519d52e2ed44e10ded0c
Junvic <junvic.valdez@instilledbee.net>
2018-07-07 20:23:03 +0800

    Migrate project and solution files to .NET Standard

--- PSXMMCLibrary.Tests/Properties/AssemblyInfo.cs
+++ PSXMMCLibrary.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PSXMMCLibrary.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © InstilledBee 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
